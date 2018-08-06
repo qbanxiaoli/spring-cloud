a43919420cc08940cb5671ded71239af560e6289
qbanxiaoli <823730820@qq.com>
2018-08-06 18:39:52 +0800

    no message

--- sms/src/main/java/com/qbanxiaoli/sms/controller/SmsManageController.java
+++ sms/src/main/java/com/qbanxiaoli/sms/controller/SmsManageController.java
@@ -10,7 +10,11 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * @author Q版小李
@@ -37,10 +41,14 @@ public class SmsManageController {
      */
     @ApiOperation(value = "获取短信验证码", notes = "用户获取短信验证码")
     @GetMapping("/sendsms/{phone}")
-    public ResponseVO sendMessage(@ApiParam(name = "phone", value = "手机号码", required = true) @Phone @PathVariable("phone") String phone) {
+    public ResponseVO sendMessage(@ApiParam(name = "phone", value = "手机号码", required = true) @Valid @Phone @PathVariable("phone") String phone, BindingResult bindingResult) {
         log.info("发送短信验证码请求");
         log.info("进行参数校验");
         // 参数处理判断
+        if(bindingResult.hasErrors()){
+            log.info("参数错误：{}", bindingResult.getFieldError().getDefaultMessage());
+            return new ResponseVO<>(SmsResponseEnum.VARIABLE, new Object[]{bindingResult.getFieldError().getDefaultMessage()});
+        }
         if (!StringUtil.isNotBlank(phone)) {
             log.info("手机号码不能为空");
             return new ResponseVO(SmsResponseEnum.PHONE_NOT_NULL);
