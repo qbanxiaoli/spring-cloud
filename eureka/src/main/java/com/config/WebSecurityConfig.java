01d3ab73c483a1067ccf461dc5dac3a0dbeec62f
qbanxiaoli <823730820@qq.com>
2018-08-07 16:51:19 +0800

    no message

--- eureka/src/main/java/com/config/WebSecurityConfig.java
+++ eureka/src/main/java/com/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author Q版小李
@@ -14,8 +15,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);//加这句是为了访问eureka控制台和/actuator时能做安全控制
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        // 如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 
 }
