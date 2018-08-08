2dbbff193bc4878bd7893c0b882f40bd6f058a69
qbanxiaoli <823730820@qq.com>
2018-08-08 17:06:36 +0800

    no message

--- common/src/main/java/com/qbanxiaoli/common/config/CorsConfig.java
+++ common/src/main/java/com/qbanxiaoli/common/config/CorsConfig.java
@@ -3,27 +3,31 @@ package com.qbanxiaoli.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author qbanxiaoli
  * @description 跨域配置
  * @create 2018/5/29
  */
+
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
-                        .allowCredentials(true).maxAge(3600);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                .allowedOrigins("*")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //设置允许的方法
+                .allowedMethods("*")
+                //跨域允许时间
+                .maxAge(3600);
     }
-
 }
