0fc76888354dd0f3c73b81e7b07599313b3db8fd
qbanxiaoli <823730820@qq.com>
2018-08-08 17:12:38 +0800

    no message

--- common/src/main/java/com/qbanxiaoli/common/config/CorsConfig.java
+++ common/src/main/java/com/qbanxiaoli/common/config/CorsConfig.java
@@ -1,11 +1,9 @@
 package com.qbanxiaoli.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author qbanxiaoli
