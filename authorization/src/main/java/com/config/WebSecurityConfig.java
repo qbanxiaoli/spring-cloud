df9edcece311a5a7c7ee472336c7e830905c9b79
qbanxiaoli <823730820@qq.com>
2019-01-14 19:22:52 +0800

    修改服务监控

--- authorization/src/main/java/com/config/WebSecurityConfig.java
+++ authorization/src/main/java/com/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
